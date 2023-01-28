0c3738a2a39c73a89731335be03316231d74b7e5
Egor <egor_panti@bk.ru>
2023-01-28 12:46:59 +0300

    Задание 31 рефакторинг

--- src/main/java/homework/lesson31/Main.java
+++ src/main/java/homework/lesson31/Main.java
@@ -1,13 +1,10 @@
 package homework.lesson31;
 
-import java.io.InputStreamReader;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(new InputStreamReader(System.in));
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
+
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
         Main.reverse(a, b);
         Main.reverseWithoutVariable(a, b);
         Main.divisionWithoutRemainder(a, b);
--- src/main/java/homework/lesson31/Task4.java
+++ src/main/java/homework/lesson31/Task4.java
@@ -7,15 +7,16 @@ public class Task4 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(new InputStreamReader(System.in));
+        System.out.println("Введите данные");
         int p = scanner.nextInt();
-        int r = scanner.nextInt();
+        double r = scanner.nextInt();
         int t = scanner.nextInt();
         Task4.incomeMoney(p, r, t);
     }
 
-    public static void incomeMoney(int p, int r, int t) {
-        int tempP = p;
-        int percents;
+    public static void incomeMoney(int p, double r, int t) {
+        double tempP = p;
+        double percents;
         for (int i = 0; i < t; i++) {
             percents = tempP * r / 100;
             tempP += percents;
