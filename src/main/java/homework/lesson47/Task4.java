922576ba697609c25adc5af208ec97fdf4552571
Egor <egor_panti@bk.ru>
2023-02-16 18:52:09 +0300

    Задание 47 рефакторинг

--- src/main/java/homework/lesson47/Task4.java
+++ src/main/java/homework/lesson47/Task4.java
@@ -1,21 +1,29 @@
 package homework.lesson47;
 
-import java.math.BigInteger;
-
 public class Task4 {
     public static void main(String[] args) {
 
         int n = Integer.parseInt(args[0]);
-        int result;
+        for (int i = 0; i < 100; i++) {
+            int result;
             do {
                 result = nextInt(n);
             } while (!isPrime(result) || result == 0);
             System.out.println(result);
+        }
     }
 
     public static boolean isPrime(int x) {
-        BigInteger bigInteger = BigInteger.valueOf(x);
-        return bigInteger.isProbablePrime((int) Math.log(x));
+        int temp;
+        boolean isPrime = true;
+        for (int i = 2; i <= x / 2; i++) {
+            temp = x % i;
+            if (temp == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        return isPrime;
     }
 
     public static int nextInt(int x) {
